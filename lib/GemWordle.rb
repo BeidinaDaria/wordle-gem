2f2a65341d1bd5df8b083679b1f249b1271dee43
MelkorTheGreatest <maiva@sfedu.ru>
2023-05-24 11:12:41 +0300

    First init

---
+++ lib/GemWordle.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require_relative "GemWordle/version"
+
+module GemWordle
+  class Error < StandardError; end
+  def self.hello
+    "hello"
+  end
+  # Your code goes here...
+end
