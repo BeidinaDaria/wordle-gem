65ebbc2075b26904f471ae8d6a0b53c232e6ae45
MelkorTheGreatest <maiva@sfedu.ru>
2023-05-30 17:32:53 +0300

    Добавил функцию для раскраски строки и вспомогательную к ней

--- lib/GemWordle.rb
+++ lib/GemWordle.rb
@@ -1,22 +1,33 @@
 # frozen_string_literal: true
 
 require_relative "GemWordle/version"
+require "colorize"
 
 module GemWordle
   class Error < StandardError; end
   def self.hello
     "hello"
   end
-  def string_compare(user_word,system_word)
-  if not (user_word.is_a?(String)) then
-    return false
+
+  def self.charcolorize(char,code)
+    case code
+    when "2"
+      char = "\e[32m" + char + "\e[0m"
+    when "1"
+      char = "\e[33m" + char + "\e[0m"
+    when "0"
+      char = "\e[37m" + char + "\e[0m"
+    end
   end
-  if not (system_word.is_a?(String)) then
-    return false
+  def self.paint(string)
+    output = ""
+    string.each_char.with_index do |char,i|
+      next if i.even?
+      output = output + charcolorize(string[i-1],char).to_s
+    end
+    return output
   end
-  user_word=user_word.downcase
-  system_word=system_word.downcase
-  return (user_word===system_word)
-end
 
 end
+  # Your code goes here...
+
